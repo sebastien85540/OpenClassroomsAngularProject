859f97a5dfee7cc66df480f371c686193a31ad1a
sebastien85540 <sebastien.reynaud123@orange.fr>
2021-11-30 17:07:18 +0100

    creation de données dynamiques de directives et de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular-openclassrooms';
+
+  lastUpdate = new Date();
+  toLastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(() => {
+        resolve(date);
+      }, 2000);
+    }
+  );
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Télévision',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  isAuth = false;
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    )
+  }
+
+  onAllumer() {
+    console.log('on allume tout !');
+  }
 }
